b81667fdd2bcd56f7d76156ff90b357d6d5ddb9d
gruault <gaspardruault2002+github@gmail.com>
2025-06-03 11:26:12 +0200

    Update programme.ino

--- programme.ino
+++ programme.ino
@@ -4,15 +4,24 @@
 #define SCREEN_WIDTH  320
 #define SCREEN_HEIGHT 240
 
-#define SCREEN_BG  TFT_BLACK
-#define ROTATION   3
-#define TEXT_SIZE  2
-#define TEXT_COLOR TFT_WHITE
+#define SCREEN_BG    TFT_BLACK
+#define ROTATION     3
+#define TEXT_SIZE    2
+#define TEXT_COLOR   TFT_WHITE
+#define LINE_SPACING 25
 
-const String MSG[3] {
-  "Fire!",
-  "Lightning!",
-  "Rumble!"
+#define MSG_SIZE 10
+const String MSG[MSG_SIZE] = {
+  "Perds 3 microcoins",
+  "Perds 2 microcoins",
+  "Passe ton tour",
+  "Va sur la case Dark Web",
+  "Donne 2 microcoins au\njoueur en possedant le\nmoins",
+  "Chaque joueur te donne 1\nmicrocoin",
+  "Reponds a une question\n \nBonne reponse : gagne 2\nmicrocoins\n \nMauvaise reponse : donne 1\nmicrocoin a chaque joueur",
+  "Gagne 2 microcoins",
+  "Gagne 3 microcoins",
+  "Cite 3 reseaux sociaux\npour gagner 5 microcoins"
 };
 
 TFT_eSPI tft;
@@ -20,24 +29,50 @@ long n;
 bool p = HIGH;
 
 void displayText(String txt, int x, int y) {
+  int i = txt.indexOf('\n');
+
   tft.fillScreen(SCREEN_BG);
-  tft.drawString(txt,x,y);
+
+  if (i == -1) {
+    tft.drawString(txt, x,y);
+    return;
+  }
+
+  do {
+    tft.drawString(txt.substring(0,i), x,y);
+    txt = txt.substring(i+1);
+    i = txt.indexOf('\n');
+    y += LINE_SPACING;
+  } while (i != -1);
+
+  tft.drawString(txt, x,y);
 }
 
 void setup() {
+  Serial.begin(9600);
+
   tft.begin();
   tft.setRotation(ROTATION);
   tft.fillScreen(SCREEN_BG);
   tft.setTextSize(TEXT_SIZE);
   tft.setTextColor(TEXT_COLOR);
+
+  randomSeed(analogRead(0));
+
   pinMode(BTN, INPUT_PULLUP);
 }
 
 void loop() {
   if (digitalRead(BTN) == LOW) {
     if (p == HIGH) {
-      n = random(0,3);
-      displayText(MSG[n],1,0);
+      n = random(0,MSG_SIZE);
+      displayText(".", 1,0);
+      delay(300);
+      displayText("..", 1,0);
+      delay(300);
+      displayText("...", 1,0);
+      delay(800);
+      displayText(MSG[n], 1,0);
     }
     p = LOW;
   } else {
